7b70253cbd14e0e1c71af8c2e8cf9a690fed7599
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-10-07 19:18:57 +0300

    Add project files.

---
+++ wpf_multi_window_app/ChildWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wpf_multi_window_app
+{
+    /// <summary>
+    /// Interaction logic for ChildWindow.xaml
+    /// </summary>
+    public partial class ChildWindow : Window
+    {
+        public string? Username { get; set; }
+        public ChildWindow()
+        {
+            InitializeComponent();
+        }
+        public ChildWindow(string username)
+        {
+            InitializeComponent();
+            this.Title = $"Hello, dear {username}";
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ShowUsername_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Username: " + Username);
+        }
+    }
+}
---
+++ wpf_multi_window_app/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf_multi_window_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenChildWindow_Click(object sender, RoutedEventArgs e)
+        {
+            // Information: you can set a startup window in App.xaml file
+
+            // create window instance
+            ChildWindow window = new ChildWindow();
+
+            // send data from parent to child window
+            // 1: using constructor
+            //ChildWindow window = new ChildWindow("Vladyslav");
+
+            // 2: using properties
+            //window.Title = "My custom title";
+            //window.Username = "prodoq";
+
+            // show window
+            if (modalRadio.IsChecked == true)
+                window.Show();
+            else
+                window.ShowDialog();
+
+            if (isWillClose.IsChecked == true)
+            {
+                this.Close();
+            }
+        }
+    }
+}
